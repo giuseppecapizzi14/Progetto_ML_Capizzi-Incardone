ab052c47decc24801a6c090aa8a86d47cafe8eae
StefanoIncardone <stefanoincardone@gmail.com>
2024-06-17 18:22:42 +0200

    Migliorata organizzazione struttura

--- SVM.py
+++ SVM.py
@@ -1,56 +1,59 @@
-from data_classes.emovo_dataset import EmovoDataset
-from extract_representetion.audio_embeddings import AudioEmbeddings
-import torch
-from torch.utils.data import DataLoader
-from yaml_config_override import add_arguments # type: ignore
+from typing import Any
+
 import numpy as np
-from sklearn.svm import SVC
+import torch
+import torch.utils.data
+from numpy.typing import NDArray
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix  # type: ignore
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.svm import SVC
+from torch import Tensor
+from torch.types import Number
+from torch.utils.data import DataLoader
 
-def process_waveform(waveform):
-    # Converte in mono tutti gli audio che non lo
-    if waveform.ndim > 1 and waveform.shape[0] > 1:
-        waveform = torch.mean(waveform, dim=0)
-    return waveform.numpy()
+from config.config import args
+from data_classes.emovo_dataset import EmovoDataset, Sample
+from extract_representetion.audio_embeddings import AudioEmbeddings
 
-def extract_embeddings_and_labels(dataloader, embeddings_extractor):
-    embeddings_list = []
-    labels_list = []
+
+def extract_embeddings_and_labels(
+    dataloader: DataLoader[Sample],
+    embeddings_extractor: AudioEmbeddings
+) -> tuple[NDArray[Any], NDArray[Any]]:
+    embeddings_list: list[Tensor] = []
+    labels_list: list[Number] = []
 
     for sample in dataloader:
-        waveform = sample["waveform"]
-        label = sample["label"]
-        
-        # Itera in ogni sample del batch
-        for i in range(waveform.size(0)): 
-            waveform_i = waveform[i]
-            label_i = label[i].item()
-            
-            waveform_i = process_waveform(waveform_i)
-            embeddings = embeddings_extractor.extract(waveform_i)
-            
-            embeddings_list.append(embeddings)
-            labels_list.append(label_i)
+        waveforms: Tensor = sample["waveform"]
+        labels: Tensor = sample["label"]
 
-    embeddings_array = np.vstack(embeddings_list)
+        for waveform, label in zip(waveforms, labels):
+            embeddings = embeddings_extractor.extract(waveform)
+            # TODO(stefano): controllare se è necessario fare questo squeeze
+            embeddings.squeeze_()
+
+            embeddings_list.append(embeddings)
+            labels_list.append(label.item())
+
+    embeddings_array = np.vstack(embeddings_list) # type: ignore
     labels_array = np.array(labels_list)
 
     return embeddings_array, labels_array
 
 if __name__ == "__main__":
     # Legge il file di configurazione
-    config = add_arguments()
+    config = args()
 
     # Carica il Dataset
     data_dir = config["data"]["data_dir"]
-    train_dataset = EmovoDataset(data_dir, train=True, resample=True)
-    test_dataset = EmovoDataset(data_dir, train=False, resample=True)
+    train_dataset = EmovoDataset(data_dir, train = True, resample = True)
+    test_dataset = EmovoDataset(data_dir, train = False, resample = True)
 
     # Carica il device da utilizzare tra CUDA, MPS e CPU
-    if config["training"]["device"] == "cuda" and torch.cuda.is_available():
+    device = config["training"]["device"]
+    if device == "cuda" and torch.cuda.is_available():
         device = torch.device("cuda")
-    elif config["training"]["device"] == "mps" and torch.backends.mps.is_available():
+    elif device == "mps" and torch.backends.mps.is_available():
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
@@ -64,51 +67,51 @@ if __name__ == "__main__":
 
     # Crea i DataLoader
     batch_size = config["training"]["batch_size"]
-    train_dl = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dl = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_dl = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_dl = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
+    val_dl = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
+    test_dl = DataLoader(test_dataset, batch_size = batch_size, shuffle = False)
 
     # Crea un'instanza della funzione di embeddings
-    embeddings_extractor = AudioEmbeddings(model_name="ALM/hubert-base-audioset", device=device)
+    embeddings_extractor = AudioEmbeddings(device=device)
 
     # Estrai embeddings e etichette dai set di train e test
-    train_embeddings_array, train_labels_array = extract_embeddings_and_labels(train_dl, embeddings_extractor)
-    test_embeddings_array, test_labels_array = extract_embeddings_and_labels(test_dl, embeddings_extractor)
+    train_embeddings, train_labels = extract_embeddings_and_labels(train_dl, embeddings_extractor)
+    test_embeddings, test_labels = extract_embeddings_and_labels(test_dl, embeddings_extractor)
 
-    print(train_embeddings_array.shape)
-    print(train_labels_array.shape)
-    print(test_embeddings_array.shape)
-    print(test_labels_array.shape)
+    print(train_embeddings.shape)
+    print(train_labels.shape)
+    print(test_embeddings.shape)
+    print(test_labels.shape)
 
     # Normalizza le embeddings
     scaler = StandardScaler()
-    train_embeddings_array = scaler.fit_transform(train_embeddings_array)
-    test_embeddings_array = scaler.transform(test_embeddings_array)
+    train_embeddings = scaler.fit_transform(train_embeddings) # type: ignore
+    test_embeddings = scaler.transform(test_embeddings) # type: ignore
 
     # Crea e addestra il classificatore SVM
-    svm_classifier = SVC(kernel='linear', C=1, random_state=42)
-    svm_classifier.fit(train_embeddings_array, train_labels_array)
-
-    # Predizioni sul set di test
-    test_predictions = svm_classifier.predict(test_embeddings_array)
+    svm_classifier = SVC(kernel = "linear", C = 1, random_state = 42)
+    svm_classifier.fit(train_embeddings, train_labels) # type: ignore
 
     # Predizioni sul set di training
-    train_predictions = svm_classifier.predict(train_embeddings_array)
-
-    # Calcola le metriche di valutazione sul set di test
-    test_accuracy = accuracy_score(test_labels_array, test_predictions)
-    test_class_report = classification_report(test_labels_array, test_predictions)
-    test_conf_matrix = confusion_matrix(test_labels_array, test_predictions)
-
-    print(f"Test Accuracy: {test_accuracy}")
-    print(f"Test Classification Report:\n{test_class_report}")
-    print(f"Test Confusion Matrix:\n{test_conf_matrix}")
+    train_predictions = svm_classifier.predict(train_embeddings) # type: ignore
 
     # Calcola le metriche di valutazione sul set di training
-    train_accuracy = accuracy_score(train_labels_array, train_predictions)
-    train_class_report = classification_report(train_labels_array, train_predictions)
-    train_conf_matrix = confusion_matrix(train_labels_array, train_predictions)
+    train_accuracy = accuracy_score(train_labels, train_predictions)
+    train_class_report = classification_report(train_labels, train_predictions) # type: ignore
+    train_conf_matrix = confusion_matrix(train_labels, train_predictions) # type: ignore
 
     print(f"Train Accuracy: {train_accuracy}")
     print(f"Train Classification Report:\n{train_class_report}")
     print(f"Train Confusion Matrix:\n{train_conf_matrix}")
+
+    # Predizioni sul set di test
+    test_predictions = svm_classifier.predict(test_embeddings) # type: ignore
+
+    # Calcola le metriche di valutazione sul set di test
+    test_accuracy = accuracy_score(test_labels, test_predictions)
+    test_class_report = classification_report(test_labels, test_predictions) # type: ignore
+    test_conf_matrix = confusion_matrix(test_labels, test_predictions) # type: ignore
+
+    print(f"Test Accuracy: {test_accuracy}")
+    print(f"Test Classification Report:\n{test_class_report}")
+    print(f"Test Confusion Matrix:\n{test_conf_matrix}")
--- extract_representetion/audio_embeddings.py
+++ extract_representetion/audio_embeddings.py
@@ -1,41 +1,39 @@
-from transformers import AutoFeatureExtractor, AutoModel
 import torch
+from torch import Tensor
+from transformers import AutoFeatureExtractor, AutoModel, PreTrainedModel, Wav2Vec2FeatureExtractor
+from transformers.modeling_outputs import BaseModelOutput
+
 from data_classes.emovo_dataset import EmovoDataset
+
 
 class AudioEmbeddings:
     """
-    This class is intended to extract embeddings from audio models.
-    It uses Wav2Vec2 as a default model.
+    Quessta classe ha il compito di estrarre gli embeddings dai modelli audio.
+    Usa il modello Wav2Vec2 come default.
     """
 
-    def __init__(self, model_name="ALM/hubert-base-audioset", device: torch.device = torch.device("cuda")):
-
-        self.processor = AutoFeatureExtractor.from_pretrained(model_name)
-        self.model = AutoModel.from_pretrained(model_name)
+    def __init__(self, device: torch.device, model_name: str = "ALM/hubert-base-audioset"):
+        self.processor: Wav2Vec2FeatureExtractor = AutoFeatureExtractor.from_pretrained(model_name) # type: ignore
+        self.model: PreTrainedModel = AutoModel.from_pretrained(model_name) # type: ignore
 
         self.device = device
-        self.model.to(self.device)
+        self.model.to(self.device) # type: ignore
 
         self.model_name = model_name
 
         # eval mode
-        self.model.eval()
+        self.model.eval() # type: ignore
 
-    def extract(self, speech): #passare waveform
-        """
-        Extract embeddings from a speech.
+    def extract(self, speech: Tensor) -> Tensor:
+        # Converte in mono l'audio se non lo è
+        if speech.ndim > 1 and speech.shape[0] > 1:
+            speech = torch.mean(speech, dim = 0)
 
-        Args:
-            speech (str): Speech to extract embeddings from.
-
-        Returns:
-            torch.Tensor: Embeddings of the speech.
-        """
-
-        inputs = self.processor(speech, return_tensors="pt", padding="longest", sampling_rate=EmovoDataset.TARGET_SAMPLE_RATE)
-        inputs = {name: tensor.to(self.device) for name, tensor in inputs.items()}
+        features = self.processor(speech.numpy(), return_tensors = "pt", sampling_rate = EmovoDataset.TARGET_SAMPLE_RATE)
+        input_values: Tensor = features.data["input_values"] # type: ignore
+        input_values = input_values.to(self.device) # type: ignore
 
         with torch.no_grad():
-            outputs = self.model(**inputs)
+            outputs: BaseModelOutput = self.model(input_values)
 
-        return outputs.last_hidden_state.mean(dim=1).detach().cpu().numpy()
+        return outputs.last_hidden_state.mean(dim = 1).cpu()
